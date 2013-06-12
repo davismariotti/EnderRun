9a1856560bfe1464200ecd4eab00d7dc0e643ea6
hawkfalcon <hawkfalcon1@gmail.com>
2013-06-12 15:27:04 -0700

    Fixed not generating world

--- src/main/java/com/hawkfalcon/enderrun/Hawky.java
+++ src/main/java/com/hawkfalcon/enderrun/Hawky.java
@@ -1,27 +1,9 @@
 package com.hawkfalcon.enderrun;
 
-import org.bukkit.generator.ChunkGenerator;
-
 import info.gomeow.enderrun.EnderRun;
 
 public class Hawky {
 
     public Hawky(EnderRun er) {
-    }
-        
-        /**
-         * 
-         * @param worldName
-         * The name of the world the generator is being applied to
-         * @param GenId
-         * The id (if any) specified by the user. It can be used if the plugin
-         * wants to have multiple generators in one plugin. More on this later.
-         * @return
-         * The ChunkGenerator that this plugin provides
-         */
-        public ChunkGenerator getDefaultWorldGenerator(String worldName, String GenId) {
-            return new com.hawkfalcon.enderrun.BasicChunkGenerator();
-        }
-        
-    
+    }    
 }
--- src/main/java/info/gomeow/enderrun/EnderRun.java
+++ src/main/java/info/gomeow/enderrun/EnderRun.java
@@ -1,5 +1,6 @@
 package info.gomeow.enderrun;
 
+import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.hawkfalcon.enderrun.Hawky;
@@ -10,4 +11,17 @@ public class EnderRun extends JavaPlugin {
         new Hawky(this);
     }
     
+    /**
+     * 
+     * @param worldName
+     * The name of the world the generator is being applied to
+     * @param GenId
+     * The id (if any) specified by the user. It can be used if the plugin
+     * wants to have multiple generators in one plugin. More on this later.
+     * @return
+     * The ChunkGenerator that this plugin provides
+     */
+    public ChunkGenerator getDefaultWorldGenerator(String worldName, String GenId) {
+        return new com.hawkfalcon.enderrun.BasicChunkGenerator();
+    }
 }
