cabccc9188b3a205cc44e27f75f68f3c0cd5f7fb
gomeow <davismariotti@gmail.com>
2013-06-13 18:10:58 -0700

    Start generation

--- src/main/java/com/github/enderrun/EnderRun.java
+++ src/main/java/com/github/enderrun/EnderRun.java
@@ -1,13 +1,30 @@
 package com.github.enderrun;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import java.io.File;
 
-import com.github.enderrun.commands.EnderRunCommandExecutor;
+import org.bukkit.World.Environment;
+import org.bukkit.WorldCreator;
+import org.bukkit.WorldType;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import com.github.enderrun.terraingen.EnderChunkGenerator;
 
 public class EnderRun extends JavaPlugin {
 
     public void onEnable() {
-        getCommand("enderrun").setExecutor(new EnderRunCommandExecutor());
+        getCommand("enderrun").setExecutor(new EnderRunCommand());
+        new BukkitRunnable() {
+
+            public void run() {
+                if(!new File("enderrun").exists()) {
+                    WorldCreator creator = new WorldCreator("enderrun");
+                    creator.environment(Environment.THE_END);
+                    creator.type(WorldType.NORMAL);
+                    creator.generateStructures(false);
+                    creator.generator(new EnderChunkGenerator());
+                }
+            }
+        }.runTaskAsynchronously(this);
     }
-    
 }
